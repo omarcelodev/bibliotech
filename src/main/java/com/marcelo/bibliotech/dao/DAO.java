e363dee8473dbf5b38d156db49d9b16fe75de0f4
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-05-13 11:49:34 -0300

    docs: Add documentacao to DAO.java

--- src/main/java/com/marcelo/bibliotech/dao/DAO.java
+++ src/main/java/com/marcelo/bibliotech/dao/DAO.java
@@ -1,6 +1,11 @@
 package com.marcelo.bibliotech.dao;
 import java.util.List;
 
+/**
+ * Define operações básicas de persistência para entidades.
+ *
+ * @param <T> tipo da entidade manipulada pelo DAO
+ */
 public interface DAO<T> {
     void save(T obj);
     T findById(int id);
