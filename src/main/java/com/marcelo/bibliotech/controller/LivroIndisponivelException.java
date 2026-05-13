77053ed105e0ce4ab41f7cab694f29107deb3597
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-05-13 12:01:54 -0300

    docs: Add docstrings to Excpetions.java

--- src/main/java/com/marcelo/bibliotech/controller/LivroIndisponivelException.java
+++ src/main/java/com/marcelo/bibliotech/controller/LivroIndisponivelException.java
@@ -1,8 +1,10 @@
 package com.marcelo.bibliotech.controller;
+/**
+ * Exceção lançada quando um livro não está disponível
+ * para empréstimo.
+ */
 public class LivroIndisponivelException extends Exception {
-
     public LivroIndisponivelException (){
-        
         super("O livro esta indisponivel no momento!");
     }
 }
